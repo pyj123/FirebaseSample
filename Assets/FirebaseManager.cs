fb407c0d598e275dd8c6349d74e1705643fe1b37
용진 <wer3799@naver.com>
2018-03-06 20:44:24 +0900

    Auth

---
+++ Assets/FirebaseManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+public class FirebaseManager : MonoBehaviour
+{
+    //이메일
+    [SerializeField]
+    InputField emailInput;
+
+    //PS
+    [SerializeField]
+    InputField psInput;
+
+
+    //디버그
+    [SerializeField]
+    Text debugText;
+    StringBuilder stringBuilder = new StringBuilder();
+
+
+    //인증 관리
+    Firebase.Auth.FirebaseAuth auth;
+
+    private void Awake()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;        
+    }    
+
+    private void AddLog(string log)
+    {
+        if (stringBuilder == null || debugText == null) return;
+        stringBuilder.Append(string.Format("{0}\n",log));
+        debugText.text = stringBuilder.ToString();
+    }
+
+    //가입버튼 클릭시
+    public void SignUp()
+    {
+        if (emailInput.text.Length == 0 || psInput.text.Length == 0) return;
+
+        auth.CreateUserWithEmailAndPasswordAsync(emailInput.text, psInput.text).ContinueWith
+            (
+            task =>
+            {
+                if(task.IsCanceled==false && task.IsFaulted == false)
+                {
+                    AddLog("가입 성공");
+                }
+                else
+                {
+                    AddLog("가입 실패");
+                }
+            }
+
+            );      
+    }
+
+    //로그인 버튼 눌렀을때
+    public void SignIn()
+    {
+        if (emailInput.text.Length == 0 || psInput.text.Length == 0) return;
+
+        auth.SignInWithEmailAndPasswordAsync(emailInput.text, psInput.text).ContinueWith
+            (
+            task =>
+            {
+                if (task.IsCompleted == true && task.IsCanceled == false && task.IsFaulted == false)
+                {
+                    Firebase.Auth.FirebaseUser newUser = task.Result;
+                    AddLog("로그인 성공");
+                }
+                else
+                {
+                    AddLog("로그인 실패");
+                }
+            }
+            );
+
+    }
+
+
+
+}
---
+++ Assets/Login.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using Firebase;
+
+public class Login : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+             .RequestIdToken()
+             .Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        string googleIdToken = ((PlayGamesLocalUser)Social.localUser).mPlatform.GetIdToken();
+
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.Credential credential =
+         Firebase.Auth.GoogleAuthProvider.GetCredential(googleIdToken, null);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+
+        
+    }
+
+ 
+}
